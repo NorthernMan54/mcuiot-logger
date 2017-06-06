256f1264f985b9c5c3fce9aadc670bb5dfeb564b
NorthernMan54 <buyminivan-northern@yahoo.ca>
2017-06-06 19:56:43 -0400

    Initial Commit

---
+++ logger.js
@@ -0,0 +1,168 @@
+var fs = require('fs');
+var readline = require('readline');
+var google = require('googleapis');
+var googleAuth = require('google-auth-library');
+var debug = require('debug')('Logger');
+
+var auth;
+var spreadsheetId;
+
+module.exports = {
+    logger: logger
+}
+
+function logger(sheetID) {
+
+    // Load client secrets from a local file.
+    spreadsheetId = sheetID;
+    fs.readFile(SECRET_PATH, function processClientSecrets(err, content) {
+        if (err) {
+            console.log('Error loading client secret file, please follow the instructions in the README!!!' + err);
+            return;
+        }
+        // Authorize a client with the loaded credentials, then call the
+        // Drive API.
+        authorize(JSON.parse(content), function(authenticated) {
+
+            auth = authenticated;
+            debug("Authenticated");
+
+        });
+    });
+
+}
+
+//{ "Hostname": "NODE-869815", "Model": "BME-GD", "Version": "1.3", "Firmware": "1.5.4", "Data": {"Temperature": 22.11, "Humidity": 42.4, "Moisture": 2, "Status": 0, "Barometer": 996.018, "Dew": 8.75, "Green": "On", "Red": "Off" }}
+
+logger.prototype.storeData = function(response) {
+
+    var sheets = google.sheets('v4');
+
+    var d = new Date();
+
+    var day = (d.getMonth() + 1).toString()+'/'+d.getDate().toString()+'/'+d.getFullYear().toString() +
+      ' '+d.getHours().toString()+':'+d.getMinutes().toString()+':'+d.getSeconds().toString();
+
+    const rows = [
+        [ day, response.Hostname, response.Model, response.Version, response.Firmware,
+            response.Data.Temperature, response.Data.Humidity, response.Data.Moisture,
+            response.Data.Status, response.Data.Barometer, response.Data.Dew,
+            response.Data.Green, response.Data.Red
+        ]
+    ];
+    const request = {
+        spreadsheetId: spreadsheetId,
+        auth: auth,
+        range: 'Sheet1', // TODO: Update placeholder value.
+        valueInputOption: 'USER_ENTERED',
+        insertDataOption: 'INSERT_ROWS', // TODO: Update placeholder value.
+        resource: {
+            values: rows
+        }
+    };
+
+    sheets.spreadsheets.values.append(request, function(err, response) {
+        if (err) {
+            console.log('The API returned an error: ' + err);
+            debug(rows);
+            return;
+        }
+        debug(response);
+    });
+
+}
+
+
+
+// If modifying these scopes, delete your previously saved credentials
+// at ~/.credentials/sheets.googleapis.com-nodejs-quickstart.json
+var SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
+var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
+    process.env.USERPROFILE) + '/.homebridge/';
+var TOKEN_PATH = TOKEN_DIR + 'sheets.googleapis.com-datalogger.json';
+var SECRET_PATH = TOKEN_DIR + 'logger_client_secret.json';
+
+// Load client secrets from a local file.
+//fs.readFile('client_secret.json', function processClientSecrets(err, content) {
+//    if (err) {
+//        console.log('Error loading client secret file: ' + err);
+//        return;
+//    }
+    // Authorize a client with the loaded credentials, then call the
+    // Google Sheets API.
+//    authorize(JSON.parse(content), listMajors);
+//});
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ *
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback) {
+    var clientSecret = credentials.installed.client_secret;
+    var clientId = credentials.installed.client_id;
+    var redirectUrl = credentials.installed.redirect_uris[0];
+    var auth = new googleAuth();
+    var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+
+    // Check if we have previously stored a token.
+    fs.readFile(TOKEN_PATH, function(err, token) {
+        if (err) {
+            getNewToken(oauth2Client, callback);
+        } else {
+            oauth2Client.credentials = JSON.parse(token);
+            callback(oauth2Client);
+        }
+    });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ *
+ * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback to call with the authorized
+ *     client.
+ */
+function getNewToken(oauth2Client, callback) {
+    var authUrl = oauth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: SCOPES
+    });
+    console.log('Authorize this app by visiting this url: ', authUrl);
+    var rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    rl.question('Enter the code from that page here: ', function(code) {
+        rl.close();
+        oauth2Client.getToken(code, function(err, token) {
+            if (err) {
+                console.log('Error while trying to retrieve access token', err);
+                return;
+            }
+            oauth2Client.credentials = token;
+            storeToken(token);
+            callback(oauth2Client);
+        });
+    });
+}
+
+/**
+ * Store token to disk be used in later program executions.
+ *
+ * @param {Object} token The token to store to disk.
+ */
+function storeToken(token) {
+    try {
+        fs.mkdirSync(TOKEN_DIR);
+    } catch (err) {
+        if (err.code != 'EEXIST') {
+            throw err;
+        }
+    }
+    fs.writeFile(TOKEN_PATH, JSON.stringify(token));
+    console.log('Token stored to ' + TOKEN_PATH);
+}
